8b6470fd67e3d3e78be1cebcba257c708e36053d
Xuepeisioc <65410917+Xuepeisioc@users.noreply.github.com>
2020-05-19 15:56:52 +0200

    Update fitPeptides.R

--- R/fitPeptides.R
+++ R/fitPeptides.R
@@ -16,7 +16,7 @@ fitPeptides <-
     cluster_copy(cl, "fitPeptide")
     cluster_copy(cl, "vehicle")
     cluster_copy(cl, "treatment")
-    setDefaultCluster(cl = NULL)
+    setDefaultCluster()
     if (!dir.exists(file.path(resultPath, "plots")))
       dir.create(file.path(resultPath, "plots"), recursive = TRUE)
 
